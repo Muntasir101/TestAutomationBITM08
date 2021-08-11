ad8b9541ca4ee9d7566de0a4b170c233fff68ae4
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-08-11 19:57:43 +0600

    Project Setup,Firefox Config

---
+++ src/main/java/com/BrowserSetup/BrowserConfig.java
@@ -0,0 +1,37 @@
+package com.BrowserSetup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserConfig {
+
+    public static WebDriver driver;
+
+    public static void main(String[] args)
+    {
+        firefox_launch();
+        firefox_close();
+    }
+
+    public static void firefox_launch()
+    {
+        System.setProperty("webdriver.gecko.driver","./src/main/resources/geckodriver.exe");
+        driver=new FirefoxDriver();
+    }
+
+    public static void firefox_close(){
+        driver.close();
+    }
+
+    public static void chrome_launch()
+    {
+
+    }
+
+    public static void edge_launch()
+    {
+
+    }
+
+
+}
